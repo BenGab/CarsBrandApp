0aac8706bf10cc42da79760c794037ba95f357f3
Gabor Benko <benkogabor@msn.com>
2020-09-27 19:51:50 +0200

    base model creation

---
+++ CarsBrand.Models/Brand.cs
@@ -0,0 +1,9 @@
+﻿namespace CarsBrand.Models
+{
+    public class Brand
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ CarsBrand.Models/Car.cs
@@ -0,0 +1,13 @@
+﻿namespace CarsBrand.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+
+        public Brand Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public decimal BasePrice { get; set; }
+    }
+}
